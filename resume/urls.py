61cd88cb872678aa008fbffd4085abfed1ea632c
Hoda Sheikhany <1385.hoda.sh@gmail.com>
2024-12-06 17:54:07 +0330

    add base.html for extends and header.html for including

--- resume/urls.py
+++ resume/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path  
-from .views import home  
+from .views import home, ADMIN_CHAT  
 
 urlpatterns = [  
     path('', home, name='home'), 
+    path('contact/', ADMIN_CHAT, name='ADMIN_CHAT'), 
 ]
--- resume/views.py
+++ resume/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, "home.html")
 
+def ADMIN_CHAT(request):
+    return render(request,'chat.html')
